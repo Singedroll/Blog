cbde43f3bff3ebcddd5be6c4d70528f941972c78
singedroll <otgonnaran@gmail.com>
2024-09-05 12:32:38 +0800

    update

--- src/components/BlogFooter.js
+++ src/components/BlogFooter.js
@@ -2,46 +2,61 @@ import { FacebookIcon } from "../components/icons/FacebookIcon";
 import { TweeterIcon } from "../components/icons/TweeterIcon";
 import { InstagramIcon } from "../components/icons/InstagramIcon";
 import { LnIcon } from "./icons/LnIcon";
+import { BlogIcon } from "./icons/BlogIcon";
+
 export const BlogFooter = () => {
   return (
-    <div className="flex justify-between">
-      <div className="flex flex-col gap-6 w-[289px]">
-        <div className="flex flex-col gap-3">
-          <p className="text-lg font-semibold text-[#181A2A]">About</p>
-          <p className="text-[#696A75] text-base flex-wrap">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam
+    <div className="flex flex-col gap-[25px]">
+      <div className="flex w-full gap-5 ">
+        <div className="flex flex-col gap-6 w-[289px]">
+          <div className="flex flex-col gap-3">
+            <p className="text-lg font-semibold text-[#181A2A]">About</p>
+            <p className="text-[#696A75] text-base flex-wrap">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam
+            </p>
+          </div>
+          <div className="flex flex-col gap-1">
+            <div className="flex items-center">
+              <span className="mr-2 text-[#181a2a] text-base font-normal font-work-sans leading-normal">
+                Email:
+              </span>
+              <span className="text-[#3b3c4a] text-base font-normal font-work-sans leading-normal">
+                info@jstemplate.net
+              </span>
+            </div>
+            <p className="font-work-sans">Phone: 880 123 456 789</p>
+          </div>
+        </div>
+        <div className="flex flex-col gap-2 items-center w-[521px]">
+          <p className="font-work-sans font-normal text-base text-[#3b3c4a]">
+            Home
+          </p>
+          <p className="font-work-sans font-normal text-base text-[#3b3c4a]">
+            Blog
+          </p>
+          <p className="font-work-sans font-normal text-base text-[#3b3c4a]">
+            Contact
           </p>
         </div>
-        <div className="flex flex-col gap-1">
-          <div className="flex items-center">
-            <span className="mr-2 text-[#181a2a] text-base font-normal font-work-sans leading-normal">
-              Email:
-            </span>
-            <span className="text-[#3b3c4a] text-base font-normal font-work-sans leading-normal">
-              info@jstemplate.net
-            </span>
-          </div>
-          <p className="font-work-sans">Phone: 880 123 456 789</p>
+        <div className="flex w-36 justify-between h-4">
+          <FacebookIcon />
+          <TweeterIcon />
+          <InstagramIcon />
+          <LnIcon />
         </div>
       </div>
-      <div className="flex flex-col gap-2">
-        <p className="font-work-sans font-normal text-base text-[#3b3c4a]">
-          Home
-        </p>
-        <p className="font-work-sans font-normal text-base text-[#3b3c4a]">
-          Blog
-        </p>
-        <p className="font-work-sans font-normal text-base text-[#3b3c4a]">
-          Contact
-        </p>
-      </div>
-      <div className="flex w-36 justify-between h-4">
-        <FacebookIcon />
-        <TweeterIcon />
-        <InstagramIcon />
-        <LnIcon />
+      <div className="flex w-full py-8 px-0">
+        <div className="flex justify-center items-center">
+          <div className="flex gap-[10px]">
+            <BlogIcon />
+            <p className="">
+              Meta<span className="font-bold">Blog</span>
+              <p>© All Rights Reserved.</p>
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
---
+++ src/pages/blog/[slug].js
@@ -0,0 +1,8 @@
+import useSWR from "swr";
+import { parse } from "html-react-parser";
+
+const url = "https://dev.to/api/articles/150589";
+
+export const Slug = () => {
+  return <div></div>;
+};
